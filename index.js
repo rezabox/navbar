1c0d40b4a8faf5af685803a3374949b1ac68aafd
rezabox <reza.asareh81@gmail.com>
2021-12-08 15:23:04 +0330

    start project

---
+++ index.js
@@ -0,0 +1,6 @@
+function toggel(){
+     let navbar = document.querySelector('#navbar');
+     let toggel = document.querySelector('#toggel');
+     navbar.classList.toggle('active');
+     toggel.classList.toggle('active');
+}
